5bf752784e0807f5ffc807faece318caadcac9f2
Jake Wilkins <jakewilkins@github.com>
2021-12-16 08:38:01 +0000

    templated 9

---
+++ 9/2.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative "../toolkit"
+
+class PuzzleTwo < Puzzle
+  state :state, nil
+
+  def parse_inputs
+    print "Wait..."
+  end
+
+  def calculate
+    puts "let me think!"
+  end
+
+  def post_process
+    puts "nvm"
+  end
+end
